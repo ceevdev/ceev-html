fe4a6f018c5d63c84fbe5c42f327850a11759e36
ceevdev <colegioernanividaldev@gmail.com>
2024-08-12 23:44:16 -0300

    V2.1 - Prototype

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,31 +1,41 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+let slideIndex = 0;
+let autoSlideTimeout;
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+
+  // Reset the slide index if out of bounds
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+
+  // Hide all slides and remove active class from dots
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // Show the current slide and add active class to the corresponding dot
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+
+  // Clear the previous timeout and set a new one for automatic sliding
+  clearTimeout(autoSlideTimeout);
+  autoSlideTimeout = setTimeout(() => plusSlides(1), 5000); // Change slide every 5 seconds
 }
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Start the slideshow
+showSlides(slideIndex = 1);
